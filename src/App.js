43920bdb8ec9de51bd3c3f75fb5befd05765b48c
GeorgeCloud <net.george.sky@gmail.com>
2023-01-28 12:20:39 -0800

    display pizza toppings, if selected show in order receipt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [name, setName]           = useState('');
+  const [pepperoni, setPepperoni] = useState(false);
+  const [sausage, setSausage]     = useState(false);
+  const [peppers, setPeppers]     = useState(false);
+  const [onions, setOnions]       = useState(false);
+
+  const ingredAndSetters = {
+    'Pepperoni': [pepperoni, setPepperoni],
+    'Sausage': [sausage, setSausage],
+    'Peppers': [peppers, setPeppers],
+    'Onions': [onions, setOnions]
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="pizzaForm">
+        <h2>Pizza Order</h2>
+
+        <input type="text" name="name" />
+
+        <div className="ingredientList">
+            {Object.keys(ingredAndSetters).map(ingred => {
+              const isSelected = ingredAndSetters[ingred][0]
+              const ingredSetter = ingredAndSetters[ingred][1];
+              return (
+                <div key={ingred}>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name={ingred}
+                      value={ingred}
+                      onChange={() => ingredSetter(!isSelected)}
+                    />
+                    {ingred}
+                  </label>
+                </div>
+              )
+            })}
+          <input type="submit" />
+        </div>
+      </form>
+
+      <div className="orderReceipt">
+        <h2>Your Order</h2>
+
+        <div className="orderDetails">
+          <p>George</p>
+
+          <p>{pepperoni && "pepperoni"}</p>
+          <p>{sausage && "sausage"}</p>
+          <p>{peppers && "peppers"}</p>
+          <p>{onions && "onions"}</p>
+        </div>
+      </div>
     </div>
   );
 }
